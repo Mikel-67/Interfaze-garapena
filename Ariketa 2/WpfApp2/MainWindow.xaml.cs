addadb5f5eba0aabe6ef0ef15c682a9b70e08555
Mikel-67 <mgoyago25paag@ikzubirimanteo.com>
2025-09-12 13:45:41 +0200

    Bostgarren ariketa

---
+++ Ariketa 2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        StringBuilder frase = new StringBuilder();
+        int oraingoa = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Frase(object sender, RoutedEventArgs e)
+        {
+            frase.Append((TextInputa.Text)+ " ");
+            TextInputa.Clear();
+
+            var oraingoBotoia = (Button)this.FindName("Botoi_" + oraingoa);
+            oraingoBotoia.IsEnabled = false;
+
+            if (oraingoa < 5)
+            {
+                var hurrengoBotoia = (Button)this.FindName("Botoi_" + (oraingoa + 1));
+                hurrengoBotoia.IsEnabled = true;
+                oraingoa++;
+            }
+            else
+            {
+                TextInputa.IsEnabled = false;
+                Botoi_5.IsEnabled = false;
+                Unir.IsEnabled = true;
+                oraingoa = 1;
+            }
+        }
+
+        private void Batu(object sender, RoutedEventArgs e)
+        {
+            TextInputa.Text = frase.ToString();
+            Unir.IsEnabled = false;
+        }
+
+        private void clean(object sender, RoutedEventArgs e)
+        {
+            frase.Clear();
+            TextInputa.Clear();
+            TextInputa.IsEnabled = true;
+            Unir.IsEnabled = false;
+            for (int i = 1; i <= 5; i++)
+            {
+                var botoia = (Button)this.FindName("Botoi_" + i);
+                botoia.IsEnabled = (i == 1);
+            }
+            oraingoa = 1;
+        }
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Ariketa 5/Ariketa 5/Ariketa 5/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ariketa_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String select;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comicSans(object sender, RoutedEventArgs e)
+        {
+            hitza.FontFamily = new FontFamily("Comic Sans MS");
+        }
+        private void courier(object sender, RoutedEventArgs e)
+        {
+            hitza.FontFamily = new FontFamily("Courier New");
+        }
+        private void negrita(object sender, RoutedEventArgs e)
+        {
+            if (hitza.FontWeight == FontWeights.Bold)
+            {
+                hitza.FontWeight = FontWeights.Normal;
+            }
+            else
+            {
+                hitza.FontWeight = FontWeights.Bold;
+            }
+        }
+        private void tachado(object sender, RoutedEventArgs e)
+        {
+            if (hitza.TextDecorations == TextDecorations.Strikethrough)
+            {
+                hitza.TextDecorations = null;
+            }
+            else
+            {
+                hitza.TextDecorations = TextDecorations.Strikethrough;
+            }
+        }
+        private void tamainaHanditu(object sender, RoutedEventArgs e)
+        {
+            hitza.FontSize += 2;
+        }
+        private void cursiva(object sender, RoutedEventArgs e)
+        {
+            if (hitza.FontStyle == FontStyles.Italic)
+            {
+                hitza.FontStyle = FontStyles.Normal;
+            }
+            else
+            {
+                hitza.FontStyle = FontStyles.Italic;
+            }
+        }
+        private void subrayado(object sender, RoutedEventArgs e)
+        {
+            if (hitza.TextDecorations == TextDecorations.Underline)
+            {
+                hitza.TextDecorations = null;
+            }
+            else
+            {
+                hitza.TextDecorations = TextDecorations.Underline;
+            }
+        }
+        private void tamainaTxikitu(object sender, RoutedEventArgs e)
+        {
+            if (hitza.FontSize > 2)
+            {
+                hitza.FontSize -= 2;
+            }
+        }
+
+        private void seleccionarTexto(object sender, RoutedEventArgs e)
+        {
+            select = Box.SelectedText;
+            Info.Content = "Textua " + Box.Text.Length + " karaktere ditu, eta hautatuta dago: " + select;
+        }
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
